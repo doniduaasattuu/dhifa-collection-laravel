c77207c6e2a25983408aa0477191946c2a2ce06b
doniduaasattuu <doni.duaasattuu@gmail.com>
2023-10-01 14:50:13 +0700

    add feature delete basket

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -194,7 +194,7 @@
     @endisset
 
     <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="bg-light modal-header">
                     <h1 class=" modal-title fs-5" id="exampleModalLabel">Success! ✅</h1>
